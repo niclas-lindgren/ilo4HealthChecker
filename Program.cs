a8d5e39dc261a1639b585532cf8b3bd49b0d9974
Niclas Lindgren <niclas.lindgren@live.no>
2020-08-28 19:51:38 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iloHealthChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
