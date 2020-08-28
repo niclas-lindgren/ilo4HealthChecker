c281449d887d1110066853b0413fafc25e3e0765
Niclas Lindgren <niclas.lindgren@live.no>
2020-08-28 21:40:24 +0200

    setting up tasks

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace iloHealthChecker
 {
@@ -6,7 +8,17 @@ namespace iloHealthChecker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+            logger.LogCritical("Started");
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddConsole());
         }
     }
 }
+
