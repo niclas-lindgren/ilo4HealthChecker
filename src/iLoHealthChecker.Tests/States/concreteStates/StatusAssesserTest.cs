8becd50729e0b6445fec8416f20ebee0296039a7
Niclas Lindgren <niclas.lindgren@live.no>
2020-08-30 19:15:01 +0200

    updated folder structure

---
+++ src/iLoHealthChecker.Tests/States/concreteStates/StatusAssesserTest.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace iLoHealthChecker.Tests.States.concreteStates {
+
+    public class StatusAssesserTest {
+
+        [Fact]
+        public void state_not_ok_should_trigger_send_email_state(){
+            // arrange
+            var healthResponse = new HealthSummaryResponse();
+            var statusAsser = new StatusAssesser();
+
+            // act
+
+            // assert
+        }
+    }
+}
