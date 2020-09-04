7fe5939030b3405f5f142db48e8d13bd19e1fd53
Niclas Lindgren <niclas.lindgren@patientsky.com>
2020-09-04 20:57:31 +0200

    logging changes

--- src/iLoHealthChecker/States/concreteStates/StatusGatherer.cs
+++ src/iLoHealthChecker/States/concreteStates/StatusGatherer.cs
@@ -1,51 +1,49 @@
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-
 using iloHealthChecker.Contracts;
-
 using Newtonsoft.Json;
+using NLog;
 
 namespace iloHealthChecker.States.concreteStates
 {
     public class StatusGatherer : State
     {
-
-        private LoginResponse responseObj;
+        private readonly Logger _log;
 
         public StatusGatherer(LoginResponse responseObj)
         {
-            this.responseObj = responseObj;
+            _log = LogManager.GetCurrentClassLogger();
         }
 
         public override async Task Handle()
         {
             try
             {
-                string path = $"json/health_summary?null&_={DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}";
-                var uri = new Uri(this._stateMachine.client.BaseAddress + path);
-                System.Console.WriteLine($"Getting status");
-                var request = new HttpRequestMessage()
+                var path = $"json/health_summary?null&_={DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}";
+                var uri = new Uri(_stateMachine.client.BaseAddress + path);
+                _log.Info("Getting status");
+                var request = new HttpRequestMessage
                 {
                     RequestUri = uri,
                     Method = HttpMethod.Get,
                 };
                 request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                request.Headers.Referrer = new Uri($"{this._stateMachine.serverConfiguration.configuration.loginDetails.url}/html/info_system.html");
-                var response = await this._stateMachine.client.SendAsync(request);
+                request.Headers.Referrer = new Uri($"{_stateMachine.serverConfiguration.configuration.loginDetails.url}/html/info_system.html");
+                var response = await _stateMachine.client.SendAsync(request);
                 response.EnsureSuccessStatusCode();
                 var responseContent = response.Content.ReadAsStringAsync().Result;
                 var responseObj = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseContent);
-                System.Console.WriteLine("Getting status success");
-                this._stateMachine.TransitionTo(new StatusAssesser(responseObj));
+                _log.Info("Getting status success");
+                _stateMachine.TransitionTo(new StatusAssesser(responseObj));
             }
             catch (Exception exception)
             {
-                this._stateMachine.TransitionTo(new Failed(exception.Message));
+                _stateMachine.TransitionTo(new Failed(exception.Message));
             }
-            await this._stateMachine.Request();
+            await _stateMachine.Request();
         }
     }
 }
